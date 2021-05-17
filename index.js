cd70a9f83e32addf194b4a92db2bc12866236746
ABHISHEK SHUKLA <64950256+abhihek-enim@users.noreply.github.com>
2021-05-17 17:46:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,30 +13,30 @@ document.addEventListener("keydown", function(event){
 function makesound(key){
  switch(key){
             case "w":
-                var play= new Audio("sounds/tom-1.mp3");
+                var play= new Audio("tom-1.mp3");
                 play.play();
                 break;
             case "a":
-                var play= new Audio("sounds/tom-2.mp3");
+                var play= new Audio("tom-2.mp3");
                 play.play();
                 break;
             case "s":
-                var play= new Audio("sounds/tom-3.mp3");
+                var play= new Audio("tom-3.mp3");
                 play.play();
                 break;
             case "d":
-                var play= new Audio("sounds/tom-4.mp3");
+                var play= new Audio("tom-4.mp3");
                 play.play();
             case "j":
-                var play= new Audio("sounds/kick-bass.mp3");
+                var play= new Audio("kick-bass.mp3");
                 play.play();
                 break;
             case "k":
-                var play= new Audio("sounds/snare.mp3");
+                var play= new Audio("snare.mp3");
                 play.play();
                 break;
             case "l":
-                var play= new Audio("sounds/crash.mp3");
+                var play= new Audio("crash.mp3");
                 play.play();
                 break;
         }   
